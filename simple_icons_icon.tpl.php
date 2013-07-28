83f715b1d0f461e9eb16bc5b0ed0bd1c2274ef61
alanpeart <alan@creative.coop>
2013-07-28 11:41:26 +0100

    Starting point with first icons and basic settings

---
+++ simple_icons_icon.tpl.php
@@ -0,0 +1,5 @@
+<<?php print $wrapper ?> class="simple-icon">
+	<a href="<?php print ($link == 1) ? $cpath : '#'; ?>" title="<?php print $alt ?>">
+		<img alt="<?php print $alt; ?>" src="<?php print $icon ?>" />
+	</a>
+</<?php print $wrapper ?>>
